0428433adb6fd84ce33df31690fecab54e55a434
Tomonori Shimomura <shimomut@amazon.com>
2024-04-09 13:29:51 -0700

    Added commands - firefox/eijiro/translate

--- main.py
+++ main.py
@@ -45,7 +45,21 @@ class CraftShellApp(cmd2.Cmd):
     # --------
     # commands
 
-    argparser = cmd2.Cmd2ArgumentParser(description="Create a cluster with JSON file")
+    argparser = cmd2.Cmd2ArgumentParser(description="Launch FireFox")
+
+    @cmd2.with_category(CATEGORY_CRAFTSHELL)
+    @cmd2.with_argparser(argparser)
+    def do_firefox(self, args):
+
+        self.poutput(f"Starting FireFox")
+
+        subprocess.run(
+            ["c:/Program Files/Mozilla Firefox/firefox.exe"], 
+            cwd="c:/Program Files/Mozilla Firefox"
+            )
+
+
+    argparser = cmd2.Cmd2ArgumentParser(description="Search keywords by Google")
     argparser.add_argument("words", metavar="WORDS", nargs="+", help="Keywords to search")
 
     @cmd2.with_category(CATEGORY_CRAFTSHELL)
@@ -59,11 +73,53 @@ class CraftShellApp(cmd2.Cmd):
         webbrowser.open(url)        
 
 
-    argparser = cmd2.Cmd2ArgumentParser(description="Start the screensaver")
+    argparser = cmd2.Cmd2ArgumentParser(description="Search keywords by Eijiro")
+    argparser.add_argument("words", metavar="WORDS", nargs="+", help="Keywords to search")
+
+    @cmd2.with_category(CATEGORY_CRAFTSHELL)
+    @cmd2.with_argparser(argparser)
+    def do_eijiro(self, args):
+        query = urllib.parse.quote_plus(" ".join(args.words))
+        url = f"http://eow.alc.co.jp/search?q={query}"
+        
+        self.poutput(f"Opening {url}")
+        
+        webbrowser.open(url)        
+
+
+    argparser = cmd2.Cmd2ArgumentParser(description="Translate EN<->JA by Google Translate")
+    argparser.add_argument("words", metavar="WORDS", nargs="+", help="Text to translate")
+
+    @cmd2.with_category(CATEGORY_CRAFTSHELL)
+    @cmd2.with_argparser(argparser)
+    def do_translate(self, args):
+
+        text = " ".join(args.words)
+        
+        src_lang = "en"
+        for c in text:
+            if ord(c) > 255:
+                src_lang = "ja"
+                break
+        
+        query = urllib.parse.quote_plus(text)
+
+        if src_lang == "en":
+            url = f"https://translate.google.co.jp/#en/ja/{query}"
+        elif src_lang == "ja":
+            url = f"https://translate.google.co.jp/#ja/en/{query}"
+
+        webbrowser.open(url)        
+
+
+    argparser = cmd2.Cmd2ArgumentParser(description="Start a screensaver")
 
     @cmd2.with_category(CATEGORY_CRAFTSHELL)
     @cmd2.with_argparser(argparser)
     def do_screensaver(self, args):
+
+        self.poutput(f"Starting screensaver")
+        
         osutils.start_screensaver()
 
 
